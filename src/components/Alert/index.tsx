2c5b8c02cc573d7d535cacbec76eed2c45278b48
Josias Furtado <jo.junior94@gmail.com>
2020-06-13 18:55:14 -0300

    add componente de alerta

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import tailwind from 'tailwind-rn'
+import { Foundation } from '@expo/vector-icons'
+
+const Alert: React.FC = () => {
+  const [openAlert, setOpenAlert] = React.useState<Boolean>()
+  return (
+    <View style={tailwind('flex-row h-12 justify-end items-end')}>
+      {openAlert && (
+        <View style={tailwind('bg-white rounded-lg mr-1 py-2')}>
+          <TouchableOpacity>
+            <Text
+              allowFontScaling={false}
+              style={tailwind('font-bold text-lg px-4 mb-2')}
+            >
+              Bloqueio na via
+            </Text>
+          </TouchableOpacity>
+          <View style={tailwind('w-full border border-gray-300 mb-2')} />
+          <TouchableOpacity>
+            <Text
+              allowFontScaling={false}
+              style={tailwind('font-bold text-lg px-4 mb-2')}
+            >
+              Acidente na via
+            </Text>
+          </TouchableOpacity>
+          <View style={tailwind('w-full border border-gray-300 mb-2')} />
+          <TouchableOpacity>
+            <Text
+              allowFontScaling={false}
+              style={tailwind('font-bold text-lg px-4 mb-2')}
+            >
+              Desvio
+            </Text>
+          </TouchableOpacity>
+          <View style={tailwind('w-full border border-gray-300 mb-2')} />
+          <TouchableOpacity>
+            <Text
+              allowFontScaling={false}
+              style={tailwind('font-bold text-lg px-4 mb-2')}
+            >
+              Outro
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      <TouchableOpacity
+        onPress={() => setOpenAlert(!openAlert)}
+        style={tailwind(
+          'w-12 h-12 rounded-full bg-white items-center justify-center',
+        )}
+      >
+        <Foundation name="alert" color="#FF1E1E" size={35} />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Alert
